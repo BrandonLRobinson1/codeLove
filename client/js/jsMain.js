e2f9985037f6435a569d9fc29d4d093e8b4c7d3a
Brandon Robinson <BrandonLRobinson1@gmail.com>
2018-12-29 19:21:27 -0500

    real deal js here baby

--- client/js/jsMain.js
+++ client/js/jsMain.js
@@ -1,11 +1,20 @@
 console.log('🙇‍♀️ test.... icles');
 
 const createElement = (tag) => document.createElement(tag);
+const createElementWithClass = (tag, className) => {
+  const newElement = document.createElement(tag);
+  newElement.className = className;
+  return newElement;
+};
+const createElementWithAttribute = (tag, attributeType, attribute) => {
+  let newElement = document.createElement(tag);
+  newElement.setAttribute(attributeType, attribute);
+  return newElement
+};
+
 const addClass = (thing, className) => thing.classList.add(className);
 
-const form = createElement('FORM');
-addClass(form, 'superForm');
-
+const form = createElementWithClass('FORM','supaForm');
 const name = createElement('INPUT');
 const button = createElement('BUTTON');
 
