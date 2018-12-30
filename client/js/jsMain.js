9ba26e83cc1e2fa7fb7268fca2432c8e54f62998
Brandon Robinson <BrandonLRobinson1@gmail.com>
2018-12-30 14:54:38 -0500

    axios call working and bleep fetch

--- client/js/jsMain.js
+++ client/js/jsMain.js
@@ -1,5 +1,8 @@
 console.log('🙇‍♀️ test.... icles');
 
+const getByID = (name) => document.getElementById(name);
+const getByClass = (name) => document.getElementsByClassName(name); // returns array like thing so you have to isolate
+
 const createElement = (tag) => document.createElement(tag);
 const createElementWithClass = (tag, className) => {
   const newElement = document.createElement(tag);
@@ -7,7 +10,7 @@ const createElementWithClass = (tag, className) => {
   return newElement;
 };
 const createElementWithAttribute = (tag, attributeType, attribute) => {
-  let newElement = document.createElement(tag);
+  const newElement = document.createElement(tag);
   newElement.setAttribute(attributeType, attribute);
   return newElement
 };
@@ -15,7 +18,27 @@ const createElementWithAttribute = (tag, attributeType, attribute) => {
 const addClass = (thing, className) => thing.classList.add(className);
 
 const form = createElementWithClass('FORM','supaForm');
-const name = createElement('INPUT');
-const button = createElement('BUTTON');
+const name = createElementWithClass('INPUT', 'name');
+const password = createElementWithClass('INPUT', 'password');
 
-document.getElementById('form').append(form);
+const button = createElementWithClass('BUTTON', 'buttonBoys');
+button.innerHTML = 'button gawd';
+
+getByID('form').append(form);
+getByClass('supaForm')[0].append(name);
+getByClass('supaForm')[0].append(password);
+getByClass('supaForm')[0].append(button);
+
+document.getElementsByClassName('buttonBoys')[0].addEventListener("click", function(e) {
+  e.preventDefault();
+  let nameVal = name.value;
+  let passVal = password.value;
+
+  axios(`/logthename/${nameVal}`)
+    .then(data => console.log('🏀', data))
+    .catch(err => console.log('⚽', err));
+
+  name.value = '';
+  password.value = '';
+  return
+});
