ca7082312d93e95da118288316f2429f953fab6a
Brandon Robinson <BrandonLRobinson1@gmail.com>
2019-01-20 19:59:32 -0500

    find or create working

--- server/controllers/nodeAuthController.js
+++ server/controllers/nodeAuthController.js
@@ -24,31 +24,18 @@ exports.newModel = (req, res, next) => {
     });
   }
 
-  // const finalUser = User.create({});
-  // console.log('??', User.create().spread);]
-  // console.log('❓', Op)
-
   const options = {
-    // where: { ...user },
     where: {
-      [Op.or]: {
-        // password: { $ilike: user.password },
-        // email: { $ilike: user.email },
-        password: user.password,
-        email: user.email
-    }
-  },
+      ...user
+    },
     defaults: {
         email: 'Technical Lead JavaScript',
         password: 'password'
       }
   };
 
-  console.log('userrrrr', User, user);
   return User
-    .create(user)
-    // .then(() => console.log('yer'))
-    .then(() => User.findOrCreate({where: {email: 'fnord'}, defaults: {password: 'something else'}}))
+    .findOrCreate(options)
     .spread((user, created) => {
       console.log('🚗', user.get({
         plain: true
@@ -56,33 +43,6 @@ exports.newModel = (req, res, next) => {
       console.log('⭐', created)
     })
     .catch(err => console.log('err', err));
-
-  return User
-  // .findOrCreate({ user: { ...user }, defaults: {email: 'Technical Lead JavaScript'}})
-    .findOrCreate(options)
-    .spread((user, create) => {
-      console.log('byeee ----->')
-      if (create) {
-          console.log('<------- hiii')
-          return res.json({
-              created: true,
-              errorMessage: '"hi": "wtf"',
-              user: Object.assign({}, user)
-          });
-      }
-      return res.json({ created: false, errorMessage: 'Already Exists', user });
-    })
-  // .catch(err => res.status(500).json({ created: false, errorMessage: err }));
-  .catch(err => console.log('err', err));
-  // console.log('final user', finalUser);
-
-  // return
-
-  // finalUser.setPassword(user.password);
-
-  // return finalUser.save()
-  //   .then(() => res.json({ user: finalUser.toAuthJSON() }))
-  //   .catch(err => console.err('1', err));
 }
 
 exports.activatePassportValidateUser = (req, res, next) => {
