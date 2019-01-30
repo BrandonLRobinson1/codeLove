0864f48c7e5239b502d22141e7cdc4cc1f02ff33
Brandon Robinson <BrandonLRobinson1@gmail.com>
2019-01-29 21:52:15 -0500

    added babel to project, adding it to node next

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,20 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/, //**************
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              // '@babel/plugin-transform-runtime'
+            ]
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
